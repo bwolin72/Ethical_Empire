c2bbde8f135801f90eb7804bb6534bd855dfe9af
bwolin72 <106031714+bwolin72@users.noreply.github.com>
2025-06-17 02:45:44 -0700

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
