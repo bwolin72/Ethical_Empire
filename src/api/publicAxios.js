5e2686834071777dc8b3ee226c24e9a247455381
bwolin72 <106031714+bwolin72@users.noreply.github.com>
2025-06-17 02:46:16 -0700

    Update publicAxios.js
    api update

--- src/api/publicAxios.js
+++ src/api/publicAxios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const publicAxios = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
