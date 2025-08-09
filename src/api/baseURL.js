fbda8afdc4d759592a1a3b3193759c718b3d6e02
bwolin72 <bwolin72@users.noreply.github.com>
2025-08-08 18:51:05 -0700

    Account workflow

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -3,10 +3,13 @@ let baseURL = process.env.REACT_APP_API_BASE_URL;
 if (!baseURL) {
   baseURL =
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:8000/api/'
-      : 'https://api.eethmghmultimedia.com/';
+      ? 'http://localhost:8000'
+      : 'https://api.eethmghmultimedia.com';
 }
 
-if (!baseURL.endsWith('/')) baseURL += '/';
+// Remove trailing slash if exists
+if (baseURL.endsWith('/')) {
+  baseURL = baseURL.slice(0, -1);
+}
 
 export default baseURL;
