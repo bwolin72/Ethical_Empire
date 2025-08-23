d03525bf276dcfa0d791054e6dbc1600069d6a52
bwolin72 <bwolin72@users.noreply.github.com>
2025-08-23 14:22:29 -0700

    Worker Updates

--- src/api/services/contentService.js
+++ src/api/services/contentService.js
@@ -1,6 +1,6 @@
 // src/api/services/contentService.js
 import publicAxios from "../publicAxios";
-import videosAPI from "./videoAPI";
+import videosAPI from "../videoAPI";
 import mediaAPI from "../mediaAPI";
 
 // --- Normalizers: unify backend shapes ---
