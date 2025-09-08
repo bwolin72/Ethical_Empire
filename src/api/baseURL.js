815d8c1a966c252d02407c346c10d77ad130bbb3
eethm.net.sec@gmail.com <bwolin72>
2025-09-08 23:52:30 +0100

    API Content Service is frontend

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -3,8 +3,8 @@ let baseURL = process.env.REACT_APP_API_BASE_URL;
 if (!baseURL) {
   baseURL =
     process.env.NODE_ENV === "development"
-      ? "http://localhost:8000"
-      : "https://api.eethmghmultimedia.com";
+      ? "http://localhost/api:8000"
+      : "https://api.eethmghmultimedia.com/api";
 }
 
 // Remove trailing slash if exists
