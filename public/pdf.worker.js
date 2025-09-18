5e33d8852cca8c0b749052bbe5535a2f40ac8fbf
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-18 09:13:51 -0700

    Update pdf.worker.js

--- public/pdf.worker.js
+++ public/pdf.worker.js
@@ -1,1 +1,9 @@
+// pdf.worker.js
+// This file is just a simple entry point to the PDF.js worker
+// Place it in public/ for CRA compatibility
+
+import { GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf";
+
+// NOTE: When building with CRA, the worker path is relative to the public folder
+GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.js`;
 
