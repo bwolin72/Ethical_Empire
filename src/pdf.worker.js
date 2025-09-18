5b457555eae57de408e278bdb22abd33aa960a90
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-18 07:58:17 -0700

    Update pdf.worker.js

--- src/pdf.worker.js
+++ src/pdf.worker.js
@@ -1,1 +1,8 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+import { GlobalWorkerOptions } from "pdfjs-dist";
+
+// ✅ Tell pdf.js to use its own bundled worker
+import worker from "pdfjs-dist/build/pdf.worker.min.js";
+
+GlobalWorkerOptions.workerSrc = worker;
 
