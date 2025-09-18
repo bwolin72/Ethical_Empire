a62a267d69402435768806dff56380fd8c3e4b88
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-18 08:50:53 -0700

    Update pdfjs-setup.js

--- src/pdfjs-setup.js
+++ src/pdfjs-setup.js
@@ -1,1 +1,6 @@
+// pdfjs-setup.js
+import { GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf";
+
+// ✅ Use legacy UMD worker from public folder
+GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
 
