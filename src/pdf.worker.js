6fe786632309cb11ed1aa44a7ea672fe36ffd628
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-18 09:10:21 -0700

    Delete src/pdf.worker.js

--- src/pdf.worker.js
+++
@@ -1,7 +0,0 @@
-import pdfjsLib from "pdfjs-dist/legacy/build/pdf";
-
-// CRA-compatible worker
-pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
-  "./pdf.worker.min.js",
-  import.meta.url
-).href;
