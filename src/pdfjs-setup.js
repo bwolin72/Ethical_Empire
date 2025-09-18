9aaba4fabb2b413e016ba547055aa2a51573efff
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-18 10:28:59 -0700

    Update pdfjs-setup.js

--- src/pdfjs-setup.js
+++ src/pdfjs-setup.js
@@ -1,19 +1,5 @@
 // src/pdfjs-setup.js
-import { getDocument, GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf";
-import brochure from "./assets/files/brochure.pdf";
+import { GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf";
 
-// Set worker path once (before calling getDocument)
+// Set PDF.js worker
 GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
-
-// Export a function to load PDF
-export const loadPDF = async () => {
-  try {
-    const loadingTask = getDocument({ url: brochure });
-    const pdf = await loadingTask.promise;
-    console.log("PDF loaded:", pdf);
-    return pdf;
-  } catch (err) {
-    console.error("Error loading PDF:", err);
-    throw err;
-  }
-};
