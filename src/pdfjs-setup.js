837bc635d7a94f2f394f542a3342c72eb3ae9600
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-18 09:29:32 -0700

    Update pdfjs-setup.js

--- src/pdfjs-setup.js
+++ src/pdfjs-setup.js
@@ -1,6 +1,6 @@
 // src/pdfjs-setup.js
 import { GlobalWorkerOptions } from "pdfjs-dist/legacy/build/pdf";
-import PdfWorker from "./pdf.worker.js";
 
-// Configure PDF.js to use the bundled worker
-GlobalWorkerOptions.workerSrc = PdfWorker;
+// Configure PDF.js to use the worker from the public folder
+// No import from src; worker must be placed in public/pdf.worker.min.js
+GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
