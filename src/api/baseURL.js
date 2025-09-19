f93a650c6a52fd8c3e916a6f791416df9dd30830
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-19 08:56:16 -0700

    Update baseURL.js

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -3,7 +3,7 @@ let baseURL = process.env.REACT_APP_API_BASE_URL;
 if (!baseURL) {
   baseURL =
     process.env.NODE_ENV === "development"
-      ? "http://localhost/api:8000"
+      ? "http://localhost:8000/api"
       : "https://api.eethmghmultimedia.com/api";
 }
 
