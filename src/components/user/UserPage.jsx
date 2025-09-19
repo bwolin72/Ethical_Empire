250fd7a1f62cfe0d3984ac652de2fa049ced3969
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-19 07:01:55 -0700

    Update UserPage.jsx

--- src/components/user/UserPage.jsx
+++ src/components/user/UserPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
-import { useProfile } from "../../context/ProfileContext";
+import { useProfile } from "../context/ProfileContext";
 import apiService from "../../api/apiService";
 
 // Components
