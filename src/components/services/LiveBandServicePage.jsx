1a0b241842753a5d667d73ccccdd6b6b3a309f28
Ethical Empire <106031714+bwolin72@users.noreply.github.com>
2025-09-23 12:16:10 -0700

    Update LiveBandServicePage.jsx

--- src/components/services/LiveBandServicePage.jsx
+++ src/components/services/LiveBandServicePage.jsx
@@ -11,7 +11,7 @@ import Reviews from "../user/Reviews";
 import ReviewsLayout from "../user/ReviewsLayout";
 
 import useFetcher from "../../hooks/useFetcher";
-import "./liveband.css"
+import "./liveband.css";
 
 const toArray = (payload) =>
   Array.isArray(payload?.data) ? payload.data :
